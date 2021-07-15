64d6647ba31611a1e40898de329ae06ffda8735c
Bart Haasbroek <bart.haasbroek@rabobank.nl>
2021-07-15 14:37:35 +0200

    fix uppercase fn

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     const name = event.queryStringParameters.name || "World";
-    const allCaps = `Hello, ${name}`.toUppercase(),
+    const allCaps = `Hello, ${name}`.toUpperCase(),
     return {
         statusCode: 200,
         body: allCaps,
