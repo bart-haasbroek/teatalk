118b53ec5c1a0b9e8d809aadb78768f71ffcae91
Bart Haasbroek <bart.haasbroek@rabobank.nl>
2021-07-15 14:44:09 +0200

    test cloud fn

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     const name = event.queryStringParameters.name || "World";
-    const allCaps = `Hello, ${name}`.toUpperCase(),
+    const allCaps = `Hello, ${name}`.toUpperCase();
     return {
         statusCode: 200,
         body: allCaps,
